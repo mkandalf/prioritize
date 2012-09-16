a0ac42e85f0f98481d0f10fad604257f6e023e54
Brett van Zuiden <brettcvz@gmail.com>
2012-09-15 18:18:40 -0700

    Merged changes in

--- client/app.coffee
+++ client/app.coffee
@@ -9,17 +9,20 @@ window.addEventListener 'hashchange', ->
 
 # the iframe that contains the main gmail app
 MAIN_FRAME_SELECTOR = '#canvas_frame'
-PAYMENT_FIELD_REGEX = /^\$[+-]?[0-9]{1,3}(?:,?[0-9]{3})*(?:\.[0-9]{2})?\$/
+PAYMENT_FIELD_REGEX = /^\[\$[+-]?[0-9]{1,3}(?:,?[0-9]{3})*(?:\.[0-9]{2})?\]/
 
+# helper method to generate underscore template functions given its dom ID
 template = (domId) ->
   _.template ($("##{domId}").html() || "").trim()
 
+# add a <link> tag to the iframe on the gmail app
 linkCSS = ($frame) ->
   $frame.contents().find('head').append $('<link/>',
     rel: 'stylesheet'
     type: 'text/css'
     href: chrome.extension.getURL 'gmail_canvas.css'
-  )
+  ).load ->
+    console.log 'loaded css'
 
 # all kinds of payment stuffs
 payment =
@@ -34,7 +37,7 @@ payment =
     # append '$' in compose view after email actions
     # TODO: replace with handlebars template
     $actions.children().last().before(
-      '<div id="payment-button">$<input type="text" name="pay_amount" /></div>')
+      '<div id="payment-button">$<input tabindex="2" type="text" name="pay_amount" /></div>')
 
     $paybutton = $actions.find('#payment-button')
     $paybutton.on 'click', (e) ->
@@ -49,6 +52,10 @@ payment =
             console.log $data
             chrome.extension.sendMessage {method: "makePayment", to: $data.user, amount: $value}
       
+    $paymentField = $actions.find('#payment-button input')
+    # HACK: payment field isn't focusing on click by default.
+    $paymentField.on 'click', (e) -> $(this).focus()
+    $paymentField.on 'blur', @paymentFieldHandler
 
   hasCreatedPayment: false
 
@@ -57,12 +64,15 @@ payment =
     console.log amount
 
     $subject = $(MAIN_FRAME_SELECTOR).contents().find('input[name=subject]')
-
-    if hasCreatedPayment
-      $subject.val($subject.val().replace(PAYMENT_FIELD_REGEX, "$#{amount}$"))
+    if ((amount * 1) == 0) # Coerce to a number
+      # Clear the subj amount if The field is empty or a zero value
+      $subject.val($subject.val().replace(PAYMENT_FIELD_REGEX, "").trim())
+      payment.hasCreatedPayment = false
+    else if payment.hasCreatedPayment
+      $subject.val($subject.val().replace(PAYMENT_FIELD_REGEX, "[$#{amount}]"))
     else
-      hasCreatedPayment = true
-      $subject.val "$#{amount}$ #{$subject.val()}"
+      payment.hasCreatedPayment = true
+      $subject.val "[$#{amount}] #{$subject.val()}"
 
 
 inbox =
@@ -70,7 +80,7 @@ inbox =
   emails: []
   sort: =>
     canonical_table = $(MAIN_FRAME_SELECTOR).contents()
-                                            .find('table > colgroup').eq(1)
+                                            .find('table > colgroup').eq(0)
                                             .parent()
     get_emails = =>
       $emails = canonical_table.find 'tr'
@@ -79,7 +89,7 @@ inbox =
         value = subject.match PAYMENT_FIELD_REGEX
         if value?
           # strip off leading and trailing $ signs
-          value = parseFloat value[0][1..-2]
+          value = parseFloat value[0][2..-2]
         else
           value = 0
         node: $ email
@@ -88,12 +98,14 @@ inbox =
         index: i
         dest: -1
         fake: null
+        replacement: null
 
     build_fakes = =>
+      canonical_table_barebones = canonical_table.clone()
+                                    .find('tbody').empty().parent()
+                                    .css('position', 'absolute')
       get_table = ->
-        canonical_table.clone()
-                        .find('tbody').empty().parent()
-                        .css('position', 'absolute')
+        canonical_table_barebones.clone()
 
       @fakes = _(@emails).map (email) =>
         fake = get_table().find('tbody').append(email['node'].clone()).parent()
@@ -111,10 +123,66 @@ inbox =
       value_emails = _(@emails).filter (email) ->
         email.value > 0
       sorted_value_emails = _(value_emails).sortBy (email) ->
-        -1 * value
-      sorted_emails = sorted_value_emails.concat(_(@emails).without sorted_value_emails)
+        -1 * email.value
+      sorted_emails = sorted_value_emails.concat(_(@emails).difference sorted_value_emails)
       _(sorted_emails).each (email, idx) ->
-        email.index = idx
+        email.dest = idx
+    
+    animate_emails = =>
+      console.log @emails[0].node.css 'transition'
+      targets = _(@emails).pluck('node').map (node) ->
+        node[0].offsetTop
+      for email in @emails
+        email.fake.css 'top', "#{targets[email.dest]}px"
+
+    hide_fakes = =>
+      for email in @emails
+        email.node.css 'visibility', 'visible'
+      for fake in @fakes
+        fake.remove()
+    
+    move_emails = =>
+      last_email = null
+      for email in _(@emails).sortBy 'dest'
+        if not last_email?
+          canonical_table.prepend email.node
+        else
+          last_email.after email.node
+        last_email = email.node
+
+        # We need to fix clicks, because google is stupid.
+        # First, set the replacement nodes so we can find them later to simulate
+        # clicks.
+        @emails[email.dest].replacement = email.node
+        # Then intercept and redirect the clicks
+        email.node.on 'mousedown', (e, real) ->
+          if real != "fo' real"
+            e.preventDefault()
+            e.stopPropagation()
+            e.stopImmediatePropagation()
+            email.replacement.trigger 'mousedown', "fo' real"
+            false
+          else
+            true
+        email.node.on 'mouseup', (e, real) ->
+          if real != "fo' real"
+            e.preventDefault()
+            e.stopPropagation()
+            e.stopImmediatePropagation()
+            email.replacement.trigger 'mouseup', "fo' real"
+            false
+          else
+            true
+        email.node.on 'click', (e, real) ->
+          if real != "fo' real"
+            e.preventDefault()
+            e.stopPropagation()
+            e.stopImmediatePropagation()
+            email.replacement.trigger 'click', "fo' real"
+            false
+          else
+            true
+        
 
     console.log 'getting emails'
     get_emails()
@@ -123,6 +191,12 @@ inbox =
     console.log 'toggling dummies'
     toggle_fakes()
     console.log 'sorting'
+    sort_emails()
+    move_emails()
+    setTimeout animate_emails, 1000
+    setTimeout hide_fakes, 1650
+    #animate_emails()
+    #hide_fakes()
 
 modal =
   welcome: ->
@@ -130,25 +204,37 @@ modal =
     $('body').append(modal)
     $modal = $('#welcome-modal')
 
-  
+$loading = $('#loading')
+
+loadingTimer = setInterval (->
+  if $loading.css('display') == 'none'
+    inbox.sort()
+    clearInterval loadingTimer)
+  , 50
+
 $(MAIN_FRAME_SELECTOR).load ->
+  $frame = $(MAIN_FRAME_SELECTOR)
+  linkCSS($frame)
+
   console.log 'main frame loaded'
 
-  if window.location.hash.match /inbox/
-    inbox.sort()
-  else if window.location.hash.match /compose/
+  if window.location.hash.match /compose/
     payment.renderButton()
 
 
 # DOM ready
 $ ->
-    console.log "requesting needs help data"
-    chrome.extension.sendMessage {method: "getLocalStorage", key: "needsHelp"}, (response) ->
-        needsHelp =  response.data
-        console.log "needsHelp:", needsHelp
-        if false && needsHelp
-            # alert "It looks like you need help"
-            # Apply black screen on top of gmail
-            $('body').append('<div style="height: 100%; width: 100%; z-index: 1001; position: absolute; top: 0px; left: 0px; opacity: 0.5; background: #666;"></div>')
-            # Main body for content
-            $('body').append('<div style="height: 70%; width: 80%; z-index: 1002; position: absolute; top: 15%; left: 10%; background: white;"></div>')
+  console.log "requesting needs help data"
+  chrome.extension.sendRequest {
+    method: "getLocalStorage"
+  , key: "needsHelp"
+  }, (response) ->
+    needsHelp = response.data
+    console.log "needsHelp: #{needsHelp}"
+    if needsHelp
+      # alert "It looks like you need help"
+      # Apply black screen on top of gmail
+      # TODO: swap these out for underscore templates
+      $('body').append('<div style="height: 100%; width: 100%; z-index: 1001; position: absolute; top: 0px; left: 0px; opacity: 0.5; background: #666;"></div>')
+      # Main body for content
+      $('body').append('<div style="height: 70%; width: 80%; z-index: 1002; position: absolute; top: 15%; left: 10%; background: white;"></div>')
--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-  var MAIN_FRAME_SELECTOR, PAYMENT_FIELD_REGEX, inbox, linkCSS, modal, payment, template,
+  var $loading, MAIN_FRAME_SELECTOR, PAYMENT_FIELD_REGEX, inbox, linkCSS, loadingTimer, modal, payment, template,
     _this = this;
 
   console.log('Value for Gmail extension script loaded');
@@ -11,7 +11,7 @@
 
   MAIN_FRAME_SELECTOR = '#canvas_frame';
 
-  PAYMENT_FIELD_REGEX = /^\$[+-]?[0-9]{1,3}(?:,?[0-9]{3})*(?:\.[0-9]{2})?\$/;
+  PAYMENT_FIELD_REGEX = /^\[\$[+-]?[0-9]{1,3}(?:,?[0-9]{3})*(?:\.[0-9]{2})?\]/;
 
   template = function(domId) {
     return _.template(($("#" + domId).html() || "").trim());
@@ -22,22 +22,24 @@
       rel: 'stylesheet',
       type: 'text/css',
       href: chrome.extension.getURL('gmail_canvas.css')
+    }).load(function() {
+      return console.log('loaded css');
     }));
   };
 
   payment = {
     renderButton: function() {
-      var $actions, $frame, $paybutton;
+      var $actions, $frame, $paybutton, $paymentField;
       $frame = $(MAIN_FRAME_SELECTOR);
       linkCSS($frame);
       $actions = $frame.contents().find('div[role=navigation]').last().children().first();
       $actions.children('span').remove();
-      $actions.children().last().before('<div id="payment-button">$<input type="text" name="pay_amount" /></div>');
+      $actions.children().last().before('<div id="payment-button">$<input tabindex="2" type="text" name="pay_amount" /></div>');
       $paybutton = $actions.find('#payment-button');
       $paybutton.on('click', function(e) {
         return $(this).find('input').focus();
       });
-      return $paybutton.prevAll('[role=button]:contains("Send")').on('click', function(e) {
+      $paybutton.prevAll('[role=button]:contains("Send")').on('click', function(e) {
         var $to_emails, $value;
         $value = $paybutton.find('input').val();
         $to_emails = $frame.contents().find('textarea[name="to"]').val();
@@ -57,18 +59,26 @@
           });
         });
       });
+      $paymentField = $actions.find('#payment-button input');
+      $paymentField.on('click', function(e) {
+        return $(this).focus();
+      });
+      return $paymentField.on('blur', _this.paymentFieldHandler);
     },
     hasCreatedPayment: false,
     paymentFieldHandler: function(e) {
-      var $subject, amount, hasCreatedPayment;
+      var $subject, amount;
       amount = $(e.currentTarget).val();
       console.log(amount);
       $subject = $(MAIN_FRAME_SELECTOR).contents().find('input[name=subject]');
-      if (hasCreatedPayment) {
-        return $subject.val($subject.val().replace(PAYMENT_FIELD_REGEX, "$" + amount + "$"));
+      if ((amount * 1) === 0) {
+        $subject.val($subject.val().replace(PAYMENT_FIELD_REGEX, "").trim());
+        return payment.hasCreatedPayment = false;
+      } else if (payment.hasCreatedPayment) {
+        return $subject.val($subject.val().replace(PAYMENT_FIELD_REGEX, "[$" + amount + "]"));
       } else {
-        hasCreatedPayment = true;
-        return $subject.val("$" + amount + "$ " + ($subject.val()));
+        payment.hasCreatedPayment = true;
+        return $subject.val("[$" + amount + "] " + ($subject.val()));
       }
     }
   };
@@ -77,8 +87,8 @@
     fakes: [],
     emails: [],
     sort: function() {
-      var build_fakes, canonical_table, get_emails, sort_emails, toggle_fakes;
-      canonical_table = $(MAIN_FRAME_SELECTOR).contents().find('table > colgroup').eq(1).parent();
+      var animate_emails, build_fakes, canonical_table, get_emails, hide_fakes, move_emails, sort_emails, toggle_fakes;
+      canonical_table = $(MAIN_FRAME_SELECTOR).contents().find('table > colgroup').eq(0).parent();
       get_emails = function() {
         var $emails;
         $emails = canonical_table.find('tr');
@@ -87,7 +97,7 @@
           subject = ($(email)).find('td[role=link] div > span:first-child').text();
           value = subject.match(PAYMENT_FIELD_REGEX);
           if (value != null) {
-            value = parseFloat(value[0].slice(1, -1));
+            value = parseFloat(value[0].slice(2, -1));
           } else {
             value = 0;
           }
@@ -97,14 +107,16 @@
             value: value,
             index: i,
             dest: -1,
-            fake: null
+            fake: null,
+            replacement: null
           };
         });
       };
       build_fakes = function() {
-        var get_table;
+        var canonical_table_barebones, get_table;
+        canonical_table_barebones = canonical_table.clone().find('tbody').empty().parent().css('position', 'absolute');
         get_table = function() {
-          return canonical_table.clone().find('tbody').empty().parent().css('position', 'absolute');
+          return canonical_table_barebones.clone();
         };
         return _this.fakes = _(_this.emails).map(function(email) {
           var fake;
@@ -135,12 +147,91 @@
           return email.value > 0;
         });
         sorted_value_emails = _(value_emails).sortBy(function(email) {
-          return -1 * value;
+          return -1 * email.value;
         });
-        sorted_emails = sorted_value_emails.concat(_(_this.emails).without(sorted_value_emails));
+        sorted_emails = sorted_value_emails.concat(_(_this.emails).difference(sorted_value_emails));
         return _(sorted_emails).each(function(email, idx) {
-          return email.index = idx;
+          return email.dest = idx;
         });
+      };
+      animate_emails = function() {
+        var email, targets, _i, _len, _ref, _results;
+        console.log(_this.emails[0].node.css('transition'));
+        targets = _(_this.emails).pluck('node').map(function(node) {
+          return node[0].offsetTop;
+        });
+        _ref = _this.emails;
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          email = _ref[_i];
+          _results.push(email.fake.css('top', "" + targets[email.dest] + "px"));
+        }
+        return _results;
+      };
+      hide_fakes = function() {
+        var email, fake, _i, _j, _len, _len2, _ref, _ref2, _results;
+        _ref = _this.emails;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          email = _ref[_i];
+          email.node.css('visibility', 'visible');
+        }
+        _ref2 = _this.fakes;
+        _results = [];
+        for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
+          fake = _ref2[_j];
+          _results.push(fake.remove());
+        }
+        return _results;
+      };
+      move_emails = function() {
+        var email, last_email, _i, _len, _ref, _results;
+        last_email = null;
+        _ref = _(_this.emails).sortBy('dest');
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          email = _ref[_i];
+          if (!(last_email != null)) {
+            canonical_table.prepend(email.node);
+          } else {
+            last_email.after(email.node);
+          }
+          last_email = email.node;
+          _this.emails[email.dest].replacement = email.node;
+          email.node.on('mousedown', function(e, real) {
+            if (real !== "fo' real") {
+              e.preventDefault();
+              e.stopPropagation();
+              e.stopImmediatePropagation();
+              email.replacement.trigger('mousedown', "fo' real");
+              return false;
+            } else {
+              return true;
+            }
+          });
+          email.node.on('mouseup', function(e, real) {
+            if (real !== "fo' real") {
+              e.preventDefault();
+              e.stopPropagation();
+              e.stopImmediatePropagation();
+              email.replacement.trigger('mouseup', "fo' real");
+              return false;
+            } else {
+              return true;
+            }
+          });
+          _results.push(email.node.on('click', function(e, real) {
+            if (real !== "fo' real") {
+              e.preventDefault();
+              e.stopPropagation();
+              e.stopImmediatePropagation();
+              email.replacement.trigger('click', "fo' real");
+              return false;
+            } else {
+              return true;
+            }
+          }));
+        }
+        return _results;
       };
       console.log('getting emails');
       get_emails();
@@ -148,7 +239,11 @@
       build_fakes();
       console.log('toggling dummies');
       toggle_fakes();
-      return console.log('sorting');
+      console.log('sorting');
+      sort_emails();
+      move_emails();
+      setTimeout(animate_emails, 1000);
+      return setTimeout(hide_fakes, 1650);
     }
   };
 
@@ -161,25 +256,33 @@
     }
   };
 
-  $(MAIN_FRAME_SELECTOR).load(function() {
-    console.log('main frame loaded');
-    if (window.location.hash.match(/inbox/)) {
-      return inbox.sort();
-    } else if (window.location.hash.match(/compose/)) {
-      return payment.renderButton();
+  $loading = $('#loading');
+
+  loadingTimer = setInterval((function() {
+    if ($loading.css('display') === 'none') {
+      inbox.sort();
+      return clearInterval(loadingTimer);
     }
+  }), 50);
+
+  $(MAIN_FRAME_SELECTOR).load(function() {
+    var $frame;
+    $frame = $(MAIN_FRAME_SELECTOR);
+    linkCSS($frame);
+    console.log('main frame loaded');
+    if (window.location.hash.match(/compose/)) return payment.renderButton();
   });
 
   $(function() {
     console.log("requesting needs help data");
-    return chrome.extension.sendMessage({
+    return chrome.extension.sendRequest({
       method: "getLocalStorage",
       key: "needsHelp"
     }, function(response) {
       var needsHelp;
       needsHelp = response.data;
-      console.log("needsHelp:", needsHelp);
-      if (false && needsHelp) {
+      console.log("needsHelp: " + needsHelp);
+      if (needsHelp) {
         $('body').append('<div style="height: 100%; width: 100%; z-index: 1001; position: absolute; top: 0px; left: 0px; opacity: 0.5; background: #666;"></div>');
         return $('body').append('<div style="height: 70%; width: 80%; z-index: 1002; position: absolute; top: 15%; left: 10%; background: white;"></div>');
       }
